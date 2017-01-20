289620e74c1c68a7c55276c50b0604af46efdbef
Tiffany Thompson <tdthompson899@gmail.com>
2017-01-19 18:27:27 -0600

    Updated to newest version of rails - rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  # root 'welcome#index'
   root 'users#index'
   get 'users/new' => 'users#new'
   post 'users' => 'users#create'
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
   get 'sessions/new' => 'sessions#new'
   post 'sessions' => 'sessions#create'
   delete 'sessions' => 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
